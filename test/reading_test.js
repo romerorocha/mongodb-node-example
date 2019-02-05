f94bdec3cfb9f810eff81a5e48ff23b3379d712d
Claudio Romero <romerolrocha@gmail.com>
2019-02-05 11:50:44 -0300

    Remove nodemon (async issues)

--- test/reading_test.js
+++ test/reading_test.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
 const User = require('../src/user');
 
-suite('Reading users out of the database', () => {
+describe('Reading users out of the database', () => {
   let joe;
 
-  setup(async () => {
+  beforeEach(async () => {
     joe = new User({
       name: 'Joe',
     });
@@ -12,12 +12,12 @@ suite('Reading users out of the database', () => {
     await joe.save();
   });
 
-  test('finds all users with a name of joe', async () => {
+  it('finds all users with a name of joe', async () => {
     const users = await User.find({ name: 'Joe' });
     assert.equal(users[0]._id.toString(), joe._id.toString());
   });
 
-  test('find a user with a particular id', async () => {
+  it('find a user with a particular id', async () => {
     const user = await User.findOne({ _id: joe._id });
     assert.equal(user.name, 'Joe');
   });
